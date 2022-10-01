8790cc211c2dde0bbc070ab588857adbbd106cf8
Will Fitzgerald <37049+willf@users.noreply.github.com>
2022-09-30 21:16:42 -0400

    initial commit

--- src/joyfully/joyfully.py
+++ src/joyfully/joyfully.py
@@ -1,2 +1,20 @@
-def hello_world():
-    return "Hello World!"
+def hello_world(s: str = "Hello World!") -> str:
+    """
+    Returns the string "Hello World!"
+
+    >>> hello_world()
+    'Hello World!'
+
+    >>> hello_world("Hello World!")
+    'Hello World!'
+
+    >>> hello_world("Hello World")
+    'Hello World'
+
+    """
+    return s
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
